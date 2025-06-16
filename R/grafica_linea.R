aa67fd11945330447dfbe7ba00a1b2c1924534f7
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2025-06-15 23:43:41 -0600

    Updates

--- R/grafica_linea.R
+++ R/grafica_linea.R
@@ -6,6 +6,10 @@
 #' @param titulo Título del gráfico
 #' @return Un objeto ggplot
 #' @export
+#' 
+sysfonts::font_add_google("Poppins", "Poppins")
+showtext::showtext_auto()
+
 grafica_linea <- function(data, x, y, titulo = "Mi gráfico de línea bonito",subtitulo = "",
                           caption = "Fuente: Datos generados por mí") {
   ggplot2::ggplot(data, ggplot2::aes_string(x = x, y = y)) +
@@ -14,7 +18,10 @@ grafica_linea <- function(data, x, y, titulo = "Mi gráfico de línea bonito",su
     ) +
     ggplot2::theme_minimal()+
     ggplot2::theme(
-      plot.title = ggplot2::element_text(hjust = 0.5, size = 16, face = "bold"),
+      text = ggplot2::element_text(family = "Poppins"),
+      plot.title = ggplot2::element_text(hjust = 0, size = 16, face = "bold"),
+      plot.subtitle = ggplot2::element_text(hjust = 0, size = 14, face = "italic"),
+      plot.caption = ggplot2::element_text(hjust = 0, size = 12, face = "italic"),
       axis.title.x = ggplot2::element_text(size = 14),
       axis.title.y = ggplot2::element_text(size = 14),
       axis.text.x = ggplot2::element_text(size = 12),
