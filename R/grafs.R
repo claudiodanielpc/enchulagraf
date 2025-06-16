14295918f6ea110f510f4d8f8b3a60307f7f05b8
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2025-06-16 01:29:12 -0600

    updates

--- R/grafs.R
+++ R/grafs.R
@@ -19,7 +19,8 @@ grafica_linea <- function(data, x, y, titulo = "Mi gráfico de línea bonito",su
                           tam_subtitulo = 14,
                           tam_fuente = 12,
                           tam_titulo_ejes=10,
-                          tam_etiquetas_ejes = 10
+                          tam_etiquetas_ejes = 10,
+                          fuente = NULL
 ) {
   ggplot2::ggplot(data, ggplot2::aes_string(x = x, y = y)) +
     ggplot2::geom_line(color = color_linea, size = tam_linea) +
@@ -27,46 +28,7 @@ grafica_linea <- function(data, x, y, titulo = "Mi gráfico de línea bonito",su
     ) +
     ggplot2::theme_minimal()+
     ggplot2::theme(
-      plot.title = ggplot2::element_text(hjust = 0, size = tam_title, face = "bold"),
-      plot.subtitle = ggplot2::element_text(hjust = 0, size = tam_subtitulo, face = "italic"),
-      plot.caption = ggplot2::element_text(hjust = 0, size = tam_fuente),
-      axis.title.x = ggplot2::element_text(size = tam_titulo_ejes),
-      axis.title.y = ggplot2::element_text(size = tam_titulo_ejes),
-      axis.text.x = ggplot2::element_text(size = tam_etiquetas_ejes),
-      axis.text.y = ggplot2::element_text(size = tam_etiquetas_ejes)
-    )
-}
-
-#' Gráfica de barras
-#'
-#' @param data Un data.frame
-#' @param x Variable en el eje x 
-#' @param y Variable en el eje y 
-#' @param titulo Título del gráfico
-#' @return Un objeto ggplot
-#' @export
-#'
-#'
-
-grafica_barras <- function(data, x, y, 
-                            titulo = "Mi gráfico de barras bonito",
-                            subtitulo = "",
-                            titulo_x = "Eje X",
-                            titulo_y = "Eje Y",
-                           caption = "Fuente: Datos generados por mí",
-                           color_barras= "steelblue",
-                           tam_title = 16,
-                           tam_subtitulo = 14,
-                           tam_fuente = 12,
-                           tam_titulo_ejes=10,
-                           tam_etiquetas_ejes = 10
-) {
-  ggplot2::ggplot(data, ggplot2::aes_string(x = x, y = y)) +
-    ggplot2::geom_bar(stat = "identity", fill = color_barras) +
-    ggplot2::labs(title = titulo, x = titulo_x, y = titulo_y, subtitle = subtitulo, caption = caption
-    ) +
-    ggplot2::theme_minimal()+
-    ggplot2::theme(
+      text = ggplot2::element_text(family = fuente),
       plot.title = ggplot2::element_text(hjust = 0, size = tam_title, face = "bold"),
       plot.subtitle = ggplot2::element_text(hjust = 0, size = tam_subtitulo, face = "italic"),
       plot.caption = ggplot2::element_text(hjust = 0, size = tam_fuente),
